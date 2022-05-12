59df04fd89d2cb0f9935b7a69bcb9d7922fcf291
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-12 13:46:20 -0500

    Class 101: RouterModule
    - Add simple routes with AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { CountriesModule } from './countries/countries.module';
@@ -7,7 +8,7 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, CountriesModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, CountriesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
