01792a85e93affd1f21882c41f2b2f36f13be0ab
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-17 17:16:48 -0500

    Class 115: RxJs - SwitchMap Operator

--- src/app/countries/pages/show-country/show-country.component.ts
+++ src/app/countries/pages/show-country/show-country.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CountryService } from '../../services/country.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-show-country',
@@ -14,10 +15,10 @@ export class ShowCountryComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({ id }) => {
-      this.countryService.getSingleCountry(id).subscribe((country) => {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.countryService.getSingleCountry(id)))
+      .subscribe((country) => {
         console.log(country);
       });
-    });
   }
 }
