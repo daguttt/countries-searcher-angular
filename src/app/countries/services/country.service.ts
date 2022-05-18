651a5a842aecc415b7269d3a65255013bffd7761
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-17 23:36:29 -0500

    Class 126: Optimize http requests

--- src/app/countries/services/country.service.ts
+++ src/app/countries/services/country.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
@@ -9,13 +9,19 @@ import { Country } from '../interfaces/country.interface';
 export class CountryService {
   private apiUrl: string = 'https://restcountries.com/v2';
   constructor(private http: HttpClient) {}
+  get httpParams() {
+    return new HttpParams().set(
+      'fields',
+      'name,capital,flag,population,alpha2Code'
+    );
+  }
   searchByContry(countryToSearch: string): Observable<Country[]> {
     const url: string = `${this.apiUrl}/name/${countryToSearch}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
   searchByCapital(countryToSearch: string): Observable<Country[]> {
     const url: string = `${this.apiUrl}/capital/${countryToSearch}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
   getSingleCountry(id: string): Observable<Country> {
     const url: string = `${this.apiUrl}/alpha/${id}`;
@@ -23,6 +29,6 @@ export class CountryService {
   }
   searchByContinent(continentToSearch: string): Observable<Country[]> {
     const url: string = `${this.apiUrl}/region/${continentToSearch}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 }
