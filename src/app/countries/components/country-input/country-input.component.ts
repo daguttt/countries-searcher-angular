cfa88dee0aa5790e1892aa802b0f166ec6bf40ab
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-10-14 16:57:45 -0500

    fix: add unsubscribe to the debouncer

--- src/app/countries/components/country-input/country-input.component.ts
+++ src/app/countries/components/country-input/country-input.component.ts
@@ -1,26 +1,46 @@
-import { Component, EventEmitter, Output, OnInit, Input } from '@angular/core';
-import { debounceTime, Subject } from 'rxjs';
+import {
+  Component,
+  EventEmitter,
+  Output,
+  OnInit,
+  Input,
+  OnDestroy,
+} from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-country-input',
   templateUrl: './country-input.component.html',
   styles: [],
 })
-export class CountryInputComponent implements OnInit {
+export class CountryInputComponent implements OnInit, OnDestroy {
+  currentSearch = '';
+
   @Input() placeholder: string = '';
+
   @Output() onEnter: EventEmitter<string> = new EventEmitter();
   @Output() onDebounce: EventEmitter<string> = new EventEmitter();
-  public currentSearch = '';
-  public debouncer: Subject<string> = new Subject();
+
+  debouncer: Subject<string> = new Subject();
+  debouncerSubscription!: Subscription;
 
   ngOnInit(): void {
-    this.debouncer.pipe(debounceTime(300)).subscribe((search) => {
-      this.onDebounce.emit(search);
-    });
+    this.debouncerSubscription = this.debouncer
+      .pipe(debounceTime(300))
+      .subscribe((search) => {
+        this.onDebounce.emit(search);
+      });
   }
+
+  ngOnDestroy(): void {
+    console.log('Closing subscription');
+    this.debouncerSubscription.unsubscribe();
+  }
+
   searchCountry() {
     this.onEnter.emit(this.currentSearch);
   }
+
   onInput() {
     this.debouncer.next(this.currentSearch);
   }
